c62d25ba609a80250c14dc1867841695e3900bb5
hsperez6 <118867950+hsperez6@users.noreply.github.com>
2023-12-01 03:40:23 -0800

    Major Update

--- app.js
+++ app.js
@@ -22,24 +22,82 @@ console.log('Testing the connection to the database...');
 (async () => {
   try {
     // Test the connection to the database
+    await sequelize.authenticate();
     console.log('Connection to the database successful!');
 
     // Sync the models
     console.log('Synchronizing the models with the database...');
+    await sequelize.sync({ force: true });
 
     // Add People to the Database
     console.log('Adding people to the database...');
+    const peopleInstances = await Promise.all([
+      Person.create({
+        firstName: 'Brad',
+        lastName: 'Bird',
+      }),
+      Person.create({
+        firstName: 'Vin',
+        lastName: 'Diesel',
+      }),
+      Person.create({
+        firstName: 'Eli',
+        lastName: 'Marienthal',
+      }),
+      Person.create({
+        firstName: 'Craig T.',
+        lastName: 'Nelson',
+      }),
+      Person.create({
+        firstName: 'Holly',
+        lastName: 'Hunter',
+      }),      
+    ]);
+    console.log(JSON.stringify(peopleInstances, null, 2));
   
     // Update the global variables for the people instances
+    [bradBird, vinDiesel, eliMarienthal, craigTNelson, hollyHunter] = peopleInstances;
 
     // Add Movies to the Database
     console.log('Adding movies to the database...');
+    const movieInstances = await Promise.all([ 
+      Movie.create({
+        title: 'The Iron Giant',
+        releaseYear: 1999,
+        directorPersonId: bradBird.id,
+      }),
+      Movie.create({
+        title: 'The Incredibles',
+        releaseYear: 2004,
+        directorPersonId: bradBird.id,
+      }),
+    ]);
+    console.log(JSON.stringify(movieInstances, null, 2));
 
     // Retrieve movies
+    const movies = await Movie.findAll({
+      include: [
+        {
+          model: Person, 
+          as: 'director',
+        },
+      ],
+    });
+    console.log(movies.map(movie => movie.get({ plain: true })));
 
     // Retrieve people
+    const people = await Person.findAll({
+      include: [
+        {
+          model: Movie,
+          as: 'director',
+        },
+      ],
+    });
+    console.log(JSON.stringify(people, null, 2));
 
     process.exit();
+
   } catch (error) {
     if (error.name === 'SequelizeValidationError') {
       const errors = error.errors.map(err => err.message);
